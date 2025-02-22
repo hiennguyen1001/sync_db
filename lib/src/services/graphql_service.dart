7f9024307eea15face0c27343b271eebf9e8ff89
Hien nguyen <vhnguyen1001@gmail.com>
2025-02-22 22:55:46 +0700

    update request timeout to 15 seconds

--- lib/src/services/graphql_service.dart
+++ lib/src/services/graphql_service.dart
@@ -269,7 +269,7 @@ class GraphQLService extends Service {
       _graphClient = GraphQLClient(
         cache: GraphQLCache(),
         link: link,
-        queryRequestTimeout: Duration(seconds: 20)
+        queryRequestTimeout: Duration(seconds: 15)
       );
     }
 
